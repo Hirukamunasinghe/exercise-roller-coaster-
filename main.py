7552e1040325bf03ba7173d90a78ea432219f7fe
Hiru123456 <86979773+Hirukamunasinghe@users.noreply.github.com>
2021-07-07 11:26:34 +0000

    Added a bill of  for photos

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 height = int(input("What is your height?"))
+bill = 0
 if height > 120:
   print("You can ride the roller coaster")
   age = int(input("What is your age?"))
   if age <12 :
-    print("You should pay $5.")
+    bill = 5
+    print("Child tickets are $5.")
   elif age <=18 : 
-    print ("You should pay $7.")
+    bill = 7
+    print ("Youth tickets are $7.")
   else : 
-    print ("You should pay $12.")
-else :
-    print ("sorry, you should grow taller before you ride")
+    bill = 12
+    print ("Adult tickets are $12.")
+wants_photo = input ("Do you want a photo ticket? Y or N")
+if wants_photo == "Y": 
+  bill+= 3
+  print(f"Your bill is {bill}")
+else: 
+    print("Sorry, you have to grow taller.")
+
+
